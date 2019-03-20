96b4c349787eed7ccb6552007d52ef86ccd731ce
Musa Halilov <halilovmusa@gmail.com>
2019-03-20 19:52:58 +0300

    Create js dir and init vuejs project (HelloWorld)

---
+++ front-end/src/main/resources/js/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <strong class = "hw"> VueJS variable msg = {{ msg }}</strong>
+</template>
+
+<script>
+export default {
+  data(){
+  	return {
+  		msg: "Hello, World !"
+  	}
+  }
+}
+</script>
+
+<style>
+  .hw{
+    color: red;
+  }
+</style>
