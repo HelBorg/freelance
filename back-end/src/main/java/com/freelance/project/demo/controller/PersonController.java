833fcdd87fcf3cb54e28aaa6d8161e75b26137e4
User <st054388@student.spbu.ru>
2019-03-20 14:30:33 +0300

    1. Change Controller
    2. Refactoring pom.xml

---
+++ back-end/src/main/java/com/freelance/project/demo/controller/PersonController.java
@@ -0,0 +1,23 @@
+package com.freelance.project.demo.controller;
+
+import com.freelance.project.demo.dto.PersonDTO;
+import com.freelance.project.demo.service.impl.PersonServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/person")
+    public class PersonController {
+
+    @Autowired
+    private PersonServiceImpl service;
+
+    @GetMapping
+    public Collection<PersonDTO> persons() {
+        return service.findAll();
+    }
+}
