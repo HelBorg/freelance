8cc144703dc596d7a4fb733efa622ded887b26e0
Musa Halilov <halilovmusa@gmail.com>
2019-03-21 22:12:02 +0300

    add npm project and springboot files

---
+++ front-end/src/main/java/com/freelance/project/demo/FrontEndController.java
@@ -0,0 +1,11 @@
+package com.MUSA.vk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FrontEndController {
+
+}
