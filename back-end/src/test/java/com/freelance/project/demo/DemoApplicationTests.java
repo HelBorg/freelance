dbc9d669162dafe9c959303528d8547676fc56ba
Helen <st054388@student.spbu.ru>
2019-04-21 14:16:27 +0300

    Test

--- back-end/src/test/java/com/freelance/project/demo/DemoApplicationTests.java
+++ back-end/src/test/java/com/freelance/project/demo/DemoApplicationTests.java
@@ -1,16 +1,31 @@
 package com.freelance.project.demo;
 
+import com.freelance.project.demo.repository.TaskRepository;
+import com.freelance.project.demo.service.TaskService;
+import com.freelance.project.demo.service.impl.TaskServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class  DemoApplicationTests {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private TaskService taskService  = new TaskServiceImpl(taskRepository);
 
     @Test
     public void contextLoads() {
+        assertThat(taskService).isNotNull();
     }
 
 }
